62d30d0544c097ba3814562a779390ace34e0a40
Raghu Kaippully <rkaippully@gmail.com>
2019-04-02 14:12:51 +0530

    Clean up terminology. Add more tests

--- src/clj_annotations/core.clj
+++ src/clj_annotations/core.clj
@@ -1,10 +1,10 @@
 (ns clj-annotations.core
   "Access and manipulate annotations.
 
-  Annotations are arbitrary properties associated with values that can be used for a variety
-  of use cases. Typically you define a \"type\" using `defannot` specifying its attributes.
-  Each of the types and attributes can have arbitrary properties - key/value pairs - associated
-  with them.
+  Annotations are arbitrary properties associated with values that can be used for a
+  variety of use cases. Typically you define a \"schema\" using `defschema` specifying its
+  attributes Each of the schemas and attributes can have arbitrary properties - key/value
+  pairs - associated with them.
 
   This namespace provides functions and macros to define and access annotations.")
 
@@ -14,23 +14,25 @@
 ;;
 
 (defn get-annotations
-  "Access the annotations associated with a type, an attribute, and a property. Returns all
-  properties if only a type and attribute are provided. Returns nil if the specified
-  property does not exist, or the not-found value if supplied."
-  ([type attr]
-   (get-in type [:attributes attr]))
-  ([type attr prop]
-   (get-in type [:attributes attr prop]))
-  ([type attr prop not-found]
-   (get-in type [:attributes attr prop] not-found)))
+  "Access the annotations associated with a schema, an attribute, and a property. Returns
+  all properties if only `schema` and `attribute` are provided. Returns `nil` if the
+  specified property does not exist, or the `not-found` value if supplied."
+  ([schema]
+   (:attributes schema))
+  ([schema attr]
+   (get-in schema [:attributes attr]))
+  ([schema attr prop]
+   (get-in schema [:attributes attr prop]))
+  ([schema attr prop not-found]
+   (get-in schema [:attributes attr prop] not-found)))
 
-(defn get-attributes
-  "Fetch a sequence of attribute ids of a type having a specified property set to the
+(defn scan-attributes
+  "Fetch a sequence of attribute ids of a schema having a specified property set to the
   specified value."
-  [type prop value]
+  [schema prop value]
   (letfn [(prop-check [[attr props]]
             (= (get props prop) value))]
-    (map first (filter prop-check (:attributes type)))))
+    (map first (filter prop-check (:attributes schema)))))
 
 
 ;;
@@ -38,31 +40,30 @@
 ;;
 
 (defn merge-attrs
-  "a1 and a2 are annotation attribute maps of the form {:field1 m1, field2 m2, ...}
-  and {:field1 n1, :field2 n2, ...}. merge-attrs produces a map of the form
-  {:field1 (merge m1 n1), :field2 (merge m2 n2), ...}. If a field exists only in one map,
-  that value is used."
+  "a1 and a2 are annotation attribute maps of the form {:attr1 m1, :attr2 m2, ...} and
+  {:attr1 n1, :attr2 n2, ...}. merge-attrs produces a map of the form {:attr1 (merge m1
+  n1), :attr2 (merge m2 n2), ...}. If an attribute exists only in one map, that value is
+  used."
   [a1 a2]
   (merge-with merge a1 a2))
 
-(defn make-annotation
+(defn make-schema
   [{:keys [include attributes] :as m}]
   (let [attrs (merge-attrs (:attributes include) attributes)]
     (-> (merge include m)
         (dissoc :include)
         (assoc :attributes attrs)
-        (with-meta {:type ::annotation}))))
+        (with-meta {:type ::schema}))))
 
-(defmacro defannot
-  "Define annotations for a type.
+(defmacro defschema
+  "Define a schema and binds it to `name`.
 
   `kvs` are key-value pairs. The following keys are defined:
 
-  `:include`:     Include the kvs from another type to this type. If a key occurs in both
-                  the types, the one in this type wins.
-  `:name`:        A short name for this type used for documentation purposes.
-  `:description`: A description for this type used for documentation purposes.
+  `:include`:     Include the kvs from another schema to this schema. If a key occurs in
+                  both the schemas, the one in this schema wins.
+  `:name`:        A short name for this schema used for documentation purposes.
+  `:description`: A description for this schema used for documentation purposes.
   `:attributes`:  A map from attribute IDs to attribute annotations."
   [name & kvs]
-  `(defonce ~name (make-annotation (assoc {} ~@kvs))))
-
+  `(defonce ~name (make-schema (assoc {} ~@kvs))))
--- test/clj_annotations/conditions_test.clj
+++ test/clj_annotations/conditions_test.clj
@@ -1,7 +1,6 @@
 (ns clj-annotations.conditions-test
   (:require [clojure.test :refer :all]
-            [clj-annotations.conditions :as c]))
-
+            [clj-annotations.conditions :as sut]))
 
 (deftest and-test
   (letfn [(cond-success [v & ctx]
@@ -13,85 +12,85 @@
             {:errors ["error"]})]
 
     (testing "and - sss"
-      (is (= {} ((c/and cond-success cond-success cond-success) (Object.)))))
+      (is (= {} ((sut/and cond-success cond-success cond-success) (Object.)))))
 
     (testing "and - ssw"
-      (is (= {:warnings ["warning1"]} ((c/and cond-success cond-success (cond-warning 1)) (Object.)))))
+      (is (= {:warnings ["warning1"]} ((sut/and cond-success cond-success (cond-warning 1)) (Object.)))))
 
     (testing "and - sse"
-      (is (= {:errors ["error"]} ((c/and cond-success cond-success cond-error) (Object.)))))
+      (is (= {:errors ["error"]} ((sut/and cond-success cond-success cond-error) (Object.)))))
 
     (testing "and - sws"
-      (is (= {:warnings ["warning1"]} ((c/and cond-success (cond-warning 1) cond-success) (Object.)))))
+      (is (= {:warnings ["warning1"]} ((sut/and cond-success (cond-warning 1) cond-success) (Object.)))))
 
     (testing "and - sww"
-      (is (= {:warnings ["warning1"]} ((c/and cond-success (cond-warning 1) (cond-warning 2)) (Object.)))))
+      (is (= {:warnings ["warning1"]} ((sut/and cond-success (cond-warning 1) (cond-warning 2)) (Object.)))))
 
     (testing "and - swe"
-      (is (= {:warnings ["warning1"]} ((c/and cond-success (cond-warning 1) cond-error) (Object.)))))
+      (is (= {:warnings ["warning1"]} ((sut/and cond-success (cond-warning 1) cond-error) (Object.)))))
 
     (testing "and - ses"
-      (is (= {:errors ["error"]} ((c/and cond-success cond-error cond-success) (Object.)))))
+      (is (= {:errors ["error"]} ((sut/and cond-success cond-error cond-success) (Object.)))))
 
     (testing "and - sew"
-      (is (= {:errors ["error"]} ((c/and cond-success cond-error (cond-warning 1)) (Object.)))))
+      (is (= {:errors ["error"]} ((sut/and cond-success cond-error (cond-warning 1)) (Object.)))))
 
     (testing "and - see"
-      (is (= {:errors ["error"]} ((c/and cond-success cond-error cond-error) (Object.)))))
+      (is (= {:errors ["error"]} ((sut/and cond-success cond-error cond-error) (Object.)))))
 
     (testing "and - wss"
-      (is (= {:warnings ["warning1"]} ((c/and (cond-warning 1) cond-success cond-success) (Object.)))))
+      (is (= {:warnings ["warning1"]} ((sut/and (cond-warning 1) cond-success cond-success) (Object.)))))
 
     (testing "and - wsw"
-      (is (= {:warnings ["warning1"]} ((c/and (cond-warning 1) cond-success (cond-warning 2)) (Object.)))))
+      (is (= {:warnings ["warning1"]} ((sut/and (cond-warning 1) cond-success (cond-warning 2)) (Object.)))))
 
     (testing "and - wse"
-      (is (= {:warnings ["warning1"]} ((c/and (cond-warning 1) cond-success cond-error) (Object.)))))
+      (is (= {:warnings ["warning1"]} ((sut/and (cond-warning 1) cond-success cond-error) (Object.)))))
 
     (testing "and - wws"
-      (is (= {:warnings ["warning1"]} ((c/and (cond-warning 1) (cond-warning 2) cond-success) (Object.)))))
+      (is (= {:warnings ["warning1"]} ((sut/and (cond-warning 1) (cond-warning 2) cond-success) (Object.)))))
 
     (testing "and - www"
-      (is (= {:warnings ["warning1"]} ((c/and (cond-warning 1) (cond-warning 2) (cond-warning 3)) (Object.)))))
+      (is (= {:warnings ["warning1"]} ((sut/and (cond-warning 1) (cond-warning 2) (cond-warning 3)) (Object.)))))
 
     (testing "and - wwe"
-      (is (= {:warnings ["warning1"]} ((c/and (cond-warning 1) (cond-warning 2) cond-error) (Object.)))))
+      (is (= {:warnings ["warning1"]} ((sut/and (cond-warning 1) (cond-warning 2) cond-error) (Object.)))))
 
     (testing "and - wes"
-      (is (= {:warnings ["warning1"]} ((c/and (cond-warning 1) cond-error cond-success) (Object.)))))
+      (is (= {:warnings ["warning1"]} ((sut/and (cond-warning 1) cond-error cond-success) (Object.)))))
 
     (testing "and - wew"
-      (is (= {:warnings ["warning1"]} ((c/and (cond-warning 1) cond-error (cond-warning 2)) (Object.)))))
+      (is (= {:warnings ["warning1"]} ((sut/and (cond-warning 1) cond-error (cond-warning 2)) (Object.)))))
 
     (testing "and - wee"
-      (is (= {:warnings ["warning1"]} ((c/and (cond-warning 1) cond-error cond-error) (Object.)))))
+      (is (= {:warnings ["warning1"]} ((sut/and (cond-warning 1) cond-error cond-error) (Object.)))))
 
     (testing "and - ess"
-      (is (= {:errors ["error"]} ((c/and cond-error cond-success cond-success) (Object.)))))
+      (is (= {:errors ["error"]} ((sut/and cond-error cond-success cond-success) (Object.)))))
 
     (testing "and - esw"
-      (is (= {:errors ["error"]} ((c/and cond-error cond-success (cond-warning 1)) (Object.)))))
+      (is (= {:errors ["error"]} ((sut/and cond-error cond-success (cond-warning 1)) (Object.)))))
 
     (testing "and - ese"
-      (is (= {:errors ["error"]} ((c/and cond-error cond-success cond-error) (Object.)))))
+      (is (= {:errors ["error"]} ((sut/and cond-error cond-success cond-error) (Object.)))))
 
     (testing "and - ews"
-      (is (= {:errors ["error"]} ((c/and cond-error (cond-warning 1) cond-success) (Object.)))))
+      (is (= {:errors ["error"]} ((sut/and cond-error (cond-warning 1) cond-success) (Object.)))))
 
     (testing "and - eww"
-      (is (= {:errors ["error"]} ((c/and cond-error (cond-warning 1) (cond-warning 2)) (Object.)))))
+      (is (= {:errors ["error"]} ((sut/and cond-error (cond-warning 1) (cond-warning 2)) (Object.)))))
 
     (testing "and - ewe"
-      (is (= {:errors ["error"]} ((c/and cond-error (cond-warning 1) cond-error) (Object.)))))
+      (is (= {:errors ["error"]} ((sut/and cond-error (cond-warning 1) cond-error) (Object.)))))
 
     (testing "and - ees"
-      (is (= {:errors ["error"]} ((c/and cond-error cond-error cond-success) (Object.)))))
+      (is (= {:errors ["error"]} ((sut/and cond-error cond-error cond-success) (Object.)))))
 
     (testing "and - eew"
-      (is (= {:errors ["error"]} ((c/and cond-error cond-error (cond-warning 1)) (Object.)))))
+      (is (= {:errors ["error"]} ((sut/and cond-error cond-error (cond-warning 1)) (Object.)))))
 
     (testing "and - eee"
-      (is (= {:errors ["error"]} ((c/and cond-error cond-error cond-error) (Object.)))))))
+      (is (= {:errors ["error"]} ((sut/and cond-error cond-error cond-error) (Object.)))))))
 
 (deftest or-test
   (letfn [(cond-success [v & ctx]
@@ -104,152 +103,152 @@
               {:errors [(str "error" n)]}))]
 
     (testing "or - sss"
-      (is (= {} ((c/or cond-success cond-success cond-success) (Object.)))))
+      (is (= {} ((sut/or cond-success cond-success cond-success) (Object.)))))
 
     (testing "or - ssw"
-      (is (= {} ((c/or cond-success cond-success (cond-warning 1)) (Object.)))))
+      (is (= {} ((sut/or cond-success cond-success (cond-warning 1)) (Object.)))))
 
     (testing "or - sse"
-      (is (= {} ((c/or cond-success cond-success (cond-error 1)) (Object.)))))
+      (is (= {} ((sut/or cond-success cond-success (cond-error 1)) (Object.)))))
 
     (testing "or - sws"
-      (is (= {} ((c/or cond-success (cond-warning 1) cond-success) (Object.)))))
+      (is (= {} ((sut/or cond-success (cond-warning 1) cond-success) (Object.)))))
 
     (testing "or - sww"
-      (is (= {} ((c/or cond-success (cond-warning 1) (cond-warning 2)) (Object.)))))
+      (is (= {} ((sut/or cond-success (cond-warning 1) (cond-warning 2)) (Object.)))))
 
     (testing "or - swe"
-      (is (= {} ((c/or cond-success (cond-warning 1) (cond-error 1)) (Object.)))))
+      (is (= {} ((sut/or cond-success (cond-warning 1) (cond-error 1)) (Object.)))))
 
     (testing "or - ses"
-      (is (= {} ((c/or cond-success (cond-error 1) cond-success) (Object.)))))
+      (is (= {} ((sut/or cond-success (cond-error 1) cond-success) (Object.)))))
 
     (testing "or - sew"
-      (is (= {} ((c/or cond-success (cond-error 1) (cond-warning 1)) (Object.)))))
+      (is (= {} ((sut/or cond-success (cond-error 1) (cond-warning 1)) (Object.)))))
 
     (testing "or - see"
-      (is (= {} ((c/or cond-success (cond-error 1) (cond-error 2)) (Object.)))))
+      (is (= {} ((sut/or cond-success (cond-error 1) (cond-error 2)) (Object.)))))
 
     (testing "or - wss"
-      (is (= {} ((c/or (cond-warning 1) cond-success cond-success) (Object.)))))
+      (is (= {} ((sut/or (cond-warning 1) cond-success cond-success) (Object.)))))
 
     (testing "or - wsw"
-      (is (= {} ((c/or (cond-warning 1) cond-success (cond-warning 2)) (Object.)))))
+      (is (= {} ((sut/or (cond-warning 1) cond-success (cond-warning 2)) (Object.)))))
 
     (testing "or - wse"
-      (is (= {} ((c/or (cond-warning 1) cond-success (cond-error 1)) (Object.)))))
+      (is (= {} ((sut/or (cond-warning 1) cond-success (cond-error 1)) (Object.)))))
 
     (testing "or - wws"
-      (is (= {} ((c/or (cond-warning 1) (cond-warning 2) cond-success) (Object.)))))
+      (is (= {} ((sut/or (cond-warning 1) (cond-warning 2) cond-success) (Object.)))))
 
     (testing "or - www"
-      (is (= {:warnings ["warning3"]} ((c/or (cond-warning 1) (cond-warning 2) (cond-warning 3)) (Object.)))))
+      (is (= {:warnings ["warning3"]} ((sut/or (cond-warning 1) (cond-warning 2) (cond-warning 3)) (Object.)))))
 
     (testing "or - wwe"
-      (is (= {:errors ["error1"]} ((c/or (cond-warning 1) (cond-warning 2) (cond-error 1)) (Object.)))))
+      (is (= {:errors ["error1"]} ((sut/or (cond-warning 1) (cond-warning 2) (cond-error 1)) (Object.)))))
 
     (testing "or - wes"
-      (is (= {} ((c/or (cond-warning 1) (cond-error 1) cond-success) (Object.)))))
+      (is (= {} ((sut/or (cond-warning 1) (cond-error 1) cond-success) (Object.)))))
 
     (testing "or - wew"
-      (is (= {:warnings ["warning2"]} ((c/or (cond-warning 1) (cond-error 1) (cond-warning 2)) (Object.)))))
+      (is (= {:warnings ["warning2"]} ((sut/or (cond-warning 1) (cond-error 1) (cond-warning 2)) (Object.)))))
 
     (testing "or - wee"
-      (is (= {:errors ["error2"]} ((c/or (cond-warning 1) (cond-error 1) (cond-error 2)) (Object.)))))
+      (is (= {:errors ["error2"]} ((sut/or (cond-warning 1) (cond-error 1) (cond-error 2)) (Object.)))))
 
     (testing "or - ess"
-      (is (= {} ((c/or (cond-error 1) cond-success cond-success) (Object.)))))
+      (is (= {} ((sut/or (cond-error 1) cond-success cond-success) (Object.)))))
 
     (testing "or - esw"
-      (is (= {} ((c/or (cond-error 1) cond-success (cond-warning 1)) (Object.)))))
+      (is (= {} ((sut/or (cond-error 1) cond-success (cond-warning 1)) (Object.)))))
 
     (testing "or - ese"
-      (is (= {} ((c/or (cond-error 1) cond-success (cond-error 2)) (Object.)))))
+      (is (= {} ((sut/or (cond-error 1) cond-success (cond-error 2)) (Object.)))))
 
     (testing "or - ews"
-      (is (= {} ((c/or (cond-error 1) (cond-warning 1) cond-success) (Object.)))))
+      (is (= {} ((sut/or (cond-error 1) (cond-warning 1) cond-success) (Object.)))))
 
     (testing "or - eww"
-      (is (= {:warnings ["warning2"]} ((c/or (cond-error 1) (cond-warning 1) (cond-warning 2)) (Object.)))))
+      (is (= {:warnings ["warning2"]} ((sut/or (cond-error 1) (cond-warning 1) (cond-warning 2)) (Object.)))))
 
     (testing "or - ewe"
-      (is (= {:errors ["error2"]} ((c/or (cond-error 1) (cond-warning 1) (cond-error 2)) (Object.)))))
+      (is (= {:errors ["error2"]} ((sut/or (cond-error 1) (cond-warning 1) (cond-error 2)) (Object.)))))
 
     (testing "or - ees"
-      (is (= {} ((c/or (cond-error 1) (cond-error 2) cond-success) (Object.)))))
+      (is (= {} ((sut/or (cond-error 1) (cond-error 2) cond-success) (Object.)))))
 
     (testing "or - eew"
-      (is (= {:warnings ["warning1"]} ((c/or (cond-error 1) (cond-error 2) (cond-warning 1)) (Object.)))))
+      (is (= {:warnings ["warning1"]} ((sut/or (cond-error 1) (cond-error 2) (cond-warning 1)) (Object.)))))
 
     (testing "or - eee"
-      (is (= {:errors ["error3"]} ((c/or (cond-error 1) (cond-error 2) (cond-error 3)) (Object.)))))))
+      (is (= {:errors ["error3"]} ((sut/or (cond-error 1) (cond-error 2) (cond-error 3)) (Object.)))))))
 
 (deftest string-length-test
   (testing "nil string"
-    (is (= {:errors ["String length should be greater than 0"]} (c/string-length {:gt 0} nil nil))))
+    (is (= {:errors ["String length should be greater than 0"]} (sut/string-length {:gt 0} nil nil))))
   (testing "empty string"
-    (is (= {:errors ["String length should be greater than 0"]} (c/string-length {:gt 0} "" nil))))
+    (is (= {:errors ["String length should be greater than 0"]} (sut/string-length {:gt 0} "" nil))))
   (testing "lt"
-    (is (= {:errors ["String length should be less than 3"]} (c/string-length {:lt 3} "abc" nil))))
+    (is (= {:errors ["String length should be less than 3"]} (sut/string-length {:lt 3} "abc" nil))))
   (testing "le"
-    (is (= {:errors ["String length should be less than or equal to 3"]} (c/string-length {:le 3} "abcd" nil))))
+    (is (= {:errors ["String length should be less than or equal to 3"]} (sut/string-length {:le 3} "abcd" nil))))
   (testing "gt"
-    (is (= {:errors ["String length should be greater than 3"]} (c/string-length {:gt 3} "abc" nil))))
+    (is (= {:errors ["String length should be greater than 3"]} (sut/string-length {:gt 3} "abc" nil))))
   (testing "ge"
-    (is (= {:errors ["String length should be greater than or equal to 3"]} (c/string-length {:ge 3} "ab" nil))))
+    (is (= {:errors ["String length should be greater than or equal to 3"]} (sut/string-length {:ge 3} "ab" nil))))
   (testing "eq"
-    (is (= {:errors ["String length should be equal to 3"]} (c/string-length {:eq 3} "ab" nil))))
+    (is (= {:errors ["String length should be equal to 3"]} (sut/string-length {:eq 3} "ab" nil))))
   (testing "ne"
-    (is (= {:errors ["String length should not be equal to 3"]} (c/string-length {:ne 3} "abc" nil))))
+    (is (= {:errors ["String length should not be equal to 3"]} (sut/string-length {:ne 3} "abc" nil))))
   (testing "success"
-    (is (= {} (c/string-length {:ne 3} "ab" nil)))))
+    (is (= {} (sut/string-length {:ne 3} "ab" nil)))))
 
 (deftest array-length-test
   (testing "nil array"
-    (is (= {:errors ["Array length should be greater than 0"]} (c/array-length {:gt 0} nil nil))))
+    (is (= {:errors ["Array length should be greater than 0"]} (sut/array-length {:gt 0} nil nil))))
   (testing "empty array"
-    (is (= {:errors ["Array length should be greater than 0"]} (c/array-length {:gt 0} [] nil))))
+    (is (= {:errors ["Array length should be greater than 0"]} (sut/array-length {:gt 0} [] nil))))
   (testing "lt"
-    (is (= {:errors ["Array length should be less than 3"]} (c/array-length {:lt 3} [1 2 3] nil))))
+    (is (= {:errors ["Array length should be less than 3"]} (sut/array-length {:lt 3} [1 2 3] nil))))
   (testing "le"
-    (is (= {:errors ["Array length should be less than or equal to 3"]} (c/array-length {:le 3} [1 2 3 4] nil))))
+    (is (= {:errors ["Array length should be less than or equal to 3"]} (sut/array-length {:le 3} [1 2 3 4] nil))))
   (testing "gt"
-    (is (= {:errors ["Array length should be greater than 3"]} (c/array-length {:gt 3} [1 2 3] nil))))
+    (is (= {:errors ["Array length should be greater than 3"]} (sut/array-length {:gt 3} [1 2 3] nil))))
   (testing "ge"
-    (is (= {:errors ["Array length should be greater than or equal to 3"]} (c/array-length {:ge 3} [1 2] nil))))
+    (is (= {:errors ["Array length should be greater than or equal to 3"]} (sut/array-length {:ge 3} [1 2] nil))))
   (testing "eq"
-    (is (= {:errors ["Array length should be equal to 3"]} (c/array-length {:eq 3} [1 2] nil))))
+    (is (= {:errors ["Array length should be equal to 3"]} (sut/array-length {:eq 3} [1 2] nil))))
   (testing "ne"
-    (is (= {:errors ["Array length should not be equal to 3"]} (c/array-length {:ne 3} [1 2 3] nil))))
+    (is (= {:errors ["Array length should not be equal to 3"]} (sut/array-length {:ne 3} [1 2 3] nil))))
   (testing "success"
-    (is (= {} (c/array-length {:ne 3} [1 2] nil)))))
+    (is (= {} (sut/array-length {:ne 3} [1 2] nil)))))
 
 (deftest unique-attribute-test
   (testing "nil sequence"
-    (is (= {} (c/unique-attribute? :a nil nil))))
+    (is (= {} (sut/unique-attribute? :a nil nil))))
   (testing "empty sequence"
-    (is (= {} (c/unique-attribute? :a [{}] nil))))
+    (is (= {} (sut/unique-attribute? :a [{}] nil))))
   (testing "single element sequence"
-    (is (= {} (c/unique-attribute? :a [{:a 2}] nil))))
+    (is (= {} (sut/unique-attribute? :a [{:a 2}] nil))))
   (testing "no duplicates"
-    (is (= {} (c/unique-attribute? :a [{:a 2 :b 3} {:a 4 :b 3} {:a 5 :c 2}] nil))))
+    (is (= {} (sut/unique-attribute? :a [{:a 2 :b 3} {:a 4 :b 3} {:a 5 :c 2}] nil))))
   (testing "with duplicates"
-    (is (= {:errors ["Duplicate values for the attribute :a"]} (c/unique-attribute? :a [{:a 2 :b 3} {:a 4 :b 3} {:a 2 :c 2}] nil)))))
+    (is (= {:errors ["Duplicate values for the attribute :a"]} (sut/unique-attribute? :a [{:a 2 :b 3} {:a 4 :b 3} {:a 2 :c 2}] nil)))))
 
 (deftest valid-url-test
   (testing "nil url"
-    (is (= {:errors ["Malformed URL"]} (c/valid-url? nil nil))))
+    (is (= {:errors ["Malformed URL"]} (sut/valid-url? nil nil))))
   (testing "bad url"
-    (is (= {:errors ["Malformed URL"]} (c/valid-url? "blah" nil))))
+    (is (= {:errors ["Malformed URL"]} (sut/valid-url? "blah" nil))))
   (testing "good url"
-    (is (= {} (c/valid-url? "https://www.google.com" nil)))))
+    (is (= {} (sut/valid-url? "https://www.google.com" nil)))))
 
 (deftest regex-match-test
   (testing "nil regex"
-    (is (= {:errors ["Value must match the regular expression: "]} (c/regex-match? nil "abc" nil))))
+    (is (= {:errors ["Value must match the regular expression: "]} (sut/regex-match? nil "abc" nil))))
   (testing "nil string"
-    (is (= {:errors ["Value must match the regular expression: \\d{4}-\\d{2}-\\d{2}"]} (c/regex-match? #"\d{4}-\d{2}-\d{2}" nil nil))))
+    (is (= {:errors ["Value must match the regular expression: \\d{4}-\\d{2}-\\d{2}"]} (sut/regex-match? #"\d{4}-\d{2}-\d{2}" nil nil))))
   (testing "match success"
-    (is (= {} (c/regex-match? #"\d{4}-\d{2}-\d{2}" "4444-22-22" nil))))
+    (is (= {} (sut/regex-match? #"\d{4}-\d{2}-\d{2}" "4444-22-22" nil))))
   (testing "match fail"
-    (is (= {:errors ["Value must match the regular expression: \\d{4}-\\d{2}-\\d{2}"]} (c/regex-match? #"\d{4}-\d{2}-\d{2}" "44442222" nil)))))
+    (is (= {:errors ["Value must match the regular expression: \\d{4}-\\d{2}-\\d{2}"]} (sut/regex-match? #"\d{4}-\d{2}-\d{2}" "44442222" nil)))))
