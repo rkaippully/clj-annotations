2349b974840d204ca83ae40b36e022e9624ddc43
Raghu Kaippully <rkaippully@gmail.com>
2019-04-19 10:03:11 +0530

    Better signature for make-result

--- src/clj_annotations/validation.clj
+++ src/clj_annotations/validation.clj
@@ -35,46 +35,48 @@
 (def type-checks
   "A standard set of type validations used by `validate-object`. This includes validation
   for `:string`, `:boolean`, `:number`, `:date`, `:uuid`, `:url`, and `:bag` types."
-  (letfn [(f [check msg]
-            (if check
-              {}
-              {:errors [{:kind :type-mismatch :message msg}]}))]
-    {:string  #(f (instance? String %2) (str "Expected a string but found " (type-name %2)))
-     :boolean #(f (instance? Boolean %2) (str "Expected a boolean but found " (type-name %2)))
-     :number  #(f (instance? Number %2) (str "Expected a number but found " (type-name %2)))
-     :date    #(f (instance? Date %2) (str "Expected a date but found " (type-name %2)))
-     :uuid    #(f (and (string? %2) (valid-uuid? %2)) "Malformed UUID")
-     :url     #(f (and (string? %2) (valid-url? %2)) "Malformed URL")
-     :bag     #(f (map? %2) (str "Expected a map but found " (type-name %2)))}))
+  {:string  #(when-not (instance? String %2) (str "Expected a string but found " (type-name %2)))
+   :boolean #(when-not (instance? Boolean %2) (str "Expected a boolean but found " (type-name %2)))
+   :number  #(when-not (instance? Number %2) (str "Expected a number but found " (type-name %2)))
+   :date    #(when-not (instance? Date %2) (str "Expected a date but found " (type-name %2)))
+   :uuid    #(when-not (and (string? %2) (valid-uuid? %2)) "Malformed UUID")
+   :url     #(when-not (and (string? %2) (valid-url? %2)) "Malformed URL")
+   :bag     #(when-not (map? %2) (str "Expected a map but found " (type-name %2)))})
 
 (defn make-validation-result
   "Returns a standard validation result.
+
   `schema` - the schema of the attribute. optional.
-  `value` - the value of the attribute. optional
-  `path` - a sequence of attribute ids representing the location where the validation
-  failed.
-  `result` - either the output of a condition or one of the keywords below respresenting a
-  validation error."
-  [schema value path result]
-  (letfn [(cond->result [level res]
-            (let [res' (if (string? res)
-                         {:kind :validation-failure :message res}
-                         res)]
-              (assoc res' :path (reduce #(str %1 "/" %2) "" path) :level level)))
-          (error->result [kind msg]
-            [(cond->result :error {:kind    kind
-                                   :message msg})])]
-    (case result
-      :unknown-attribute-type     (error->result :unknown-attribute-type (str "Unknown attribute type: " (:type schema)))
-      :canonical-value-mismatch   (error->result :canonical-value-mismatch (str "Must be one of: " (str/join ", " (:canonical-values schema))))
-      :non-array-value            (error->result :non-array-value "Expected an array value but found a scalar")
-      :non-map-value              (error->result :non-map-value (str "Expected a map but found " (type-name value)))
-      :missing-required-attribute (error->result :missing-required-attribute "Missing required attribute")
-      :unsupported-attribute      (error->result :unsupported-attribute "Unsupported attribute")
-      ;; this is a condition result if none of the above match
+  `value`  - the value of the attribute. optional
+  `path`   - a sequence of attribute ids representing the location where the validation
+             failed.
+  `kind`   - keyword indicating the kind of failure.
+  `args`   - extra information for certain kinds of failures"
+  [schema value path kind args]
+  (letfn [(to-result [kind level msg]
+            [{:path    (reduce #(str %1 "/" %2) "" path)
+               :level   level
+               :kind    kind
+               :message msg}])]
+    (case kind
+      :unknown-attribute-type
+      (to-result :unknown-attribute-type :error (str "Unknown attribute type: " (:type schema)))
+      :canonical-value-mismatch
+      (to-result :canonical-value-mismatch :error (str "Must be one of: " (str/join ", " (:canonical-values schema))))
+      :non-array-value
+      (to-result :non-array-value :error "Expected an array value but found a scalar")
+      :non-map-value
+      (to-result :non-map-value :error (str "Expected a map but found " (type-name value)))
+      :missing-required-attribute
+      (to-result :missing-required-attribute :error "Missing required attribute")
+      :unsupported-attribute
+      (to-result :unsupported-attribute :error "Unsupported attribute")
+      :type-mismatch
+      (to-result :type-mismatch :error args)
+      :validation-failure
       (concat
-        (map #(cond->result :error %) (:errors result))
-        (map #(cond->result :warning %) (:warnings result))))))
+        (mapcat #(to-result :validation-failure :error %) (:errors args))
+        (mapcat #(to-result :validation-failure :warning %) (:warnings args))))))
 
 (def standard-opts
   "Standard options passed to `validate-object` if not overridden."
@@ -89,7 +91,7 @@
   (let [validity-fn (:validity schema)
         ctx {:path path}]
     (if-let [result (and validity-fn (validity-fn obj ctx))]
-      (make-result schema obj path result)
+      (make-result schema obj path :validation-failure result)
       [])))
 
 (declare validate-object)
@@ -105,15 +107,15 @@
       (and (= (type typ) ::core/schema))
       (validate-object path typ obj opts)
 
-      (and (contains? type-checks typ) (seq type-check-result))
-      (make-result schema obj path type-check-result)
+      (and (contains? type-checks typ) type-check-result)
+      (make-result schema obj path :type-mismatch type-check-result)
 
       (not (contains? (set (keys type-checks)) typ))
-      (make-result schema obj path :unknown-attribute-type)
+      (make-result schema obj path :unknown-attribute-type nil)
 
       ;; Validation
       (and canon-vals (not-any? (set canon-vals) [obj]))
-      (make-result schema obj path :canonical-value-mismatch)
+      (make-result schema obj path :canonical-value-mismatch nil)
 
       :else
       (eval-validity-condition path schema obj opts))))
@@ -124,7 +126,7 @@
     (letfn [(validate [i v]
               (validate-scalar-attribute (conj path i) schema v opts))]
      (apply concat (map-indexed validate obj)))
-    (make-result schema obj path :non-array-value)))
+    (make-result schema obj path :non-array-value nil)))
 
 (defn- validate-attribute
   [path
@@ -132,7 +134,7 @@
    obj
    {:keys [make-result] :as opts}]
   (cond
-    (and required (= obj ::not-found))       (make-result schema obj path :missing-required-attribute)
+    (and required (= obj ::not-found))       (make-result schema obj path :missing-required-attribute nil)
     (and (not required) (= obj ::not-found)) []
     (and (not required) (nil? obj))          []
     multi-valued                             (validate-vector-attribute path schema obj opts)
@@ -150,8 +152,8 @@
   `opts`   - options for validation, this should be a map with the following keys
 
   `:make-result` - a function that takes the schema, the attribute value, the attribute
-  path, and a validation result and returns a validation result as a vector. See
-  `make-validation-result` for an example.
+  path, an error kind with optional arguments and returns a validation result as a
+  vector. See `make-validation-result` for an example.
   `:type-checks` - a map from type names to functions validating the conformance of a
   value to those types. See `type-checks` for an example.
   `:fail-on-unsupported-attributes?` - if true an error will be reported if the `obj`
@@ -167,8 +169,7 @@
            schema-attrs      (set (keys attrs))
            unsupported-attrs (map name (cs/difference obj-attrs schema-attrs))]
        (if (and fail-on-unsupported-attributes? (seq unsupported-attrs))
-         (mapcat #(make-result schema obj (conj path (name %)) :unsupported-attribute) unsupported-attrs)
-         (mapcat #(validate-attribute (conj path (name %)) (get attrs %) (get obj % ::not-found) opts)
-           schema-attrs)))
-     (make-result schema obj path :non-map-value))))
+         (mapcat #(make-result schema obj (conj path (name %)) :unsupported-attribute nil) unsupported-attrs)
+         (mapcat #(validate-attribute (conj path (name %)) (get attrs %) (get obj % ::not-found) opts) schema-attrs)))
+     (make-result schema obj path :non-map-value nil))))
 
