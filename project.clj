5fe129f39c386aa8ad77958fb27a9e8d180e0990
Raghu Kaippully <rkaippully@gmail.com>
2019-04-26 20:24:15 +0530

    Let lein deploy use credentials from gpg

--- project.clj
+++ project.clj
@@ -14,7 +14,8 @@
              :clj08 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :clj09 {:dependencies [[org.clojure/clojure "1.9.0"]]}
              :clj10 {:dependencies [[org.clojure/clojure "1.10.0"]]}}
-  :deploy-repositories [["releases" :clojars]]
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :creds :gpg}]]
   :aliases {"update-readme-version" ["shell" "sed" "-i" "" "s/org.clojars.rkaippully\\\\/clj-annotations \"[0-9.]*\"/org.clojars.rkaippully\\\\/clj-annotations \"${:version}\"/" "README.md"]}
   :release-tasks [["kibit"]
                   ["vcs" "assert-committed"]
@@ -25,5 +26,5 @@
                   ["update-readme-version"]
                   ["vcs" "commit"]
                   ["vcs" "tag"]
-                  ["deploy" "clojars"]
+                  ["deploy"]
                   ["vcs" "push"]])
